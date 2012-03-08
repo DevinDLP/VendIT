3bbd7eaa918eb5f75f99db2758bb9d97a8792d5f
Devin de la Parte <ddelaparte@knights.ucf.edu>
2012-03-07 22:37:21 -0500

    Security warning

--- inc/globals.php
+++ inc/globals.php
@@ -7,6 +7,13 @@
 	
 	$DIR_IMG = "img/";
 	
+	/* Do not, under any circumstances, push a copy of the globals.php with your Sulley server login credentials substituted for the default ones!
+
+This is a public repository and that's a serious security risk.
+
+For this reason I encourage everyone to install and use Wamp, or your own OS's equivalent integrated server environment, for development.
+	*/
+	
 	$DB_HOST = "localhost";
 	$DB_USER = "root";
 	$DB_PASS = "password";
